dda0bdf9ac828cbf36af3d68c7737ebd5217b5f9
Septiyani Putri <80307398+septiyp@users.noreply.github.com>
2021-04-25 23:23:32 +0700

    Update Footer.js

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,26 +1,26 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Footer extends Component {
-  render(){
+  render() {
     return (
 
-      <footer class="bg-light">
-    <div class="container  text-center py-5 ">
+      <footer className="bg-light">
+        <div className="container  text-center py-5 ">
 
-      <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <b class="copyright">
-          Developer By <i class="fa fa-code text-primary mt-3"></i> Gemar Kode<br/>
-        </b>
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <b className="copyright">
+                Developer By <i className="fa fa-code text-primary mt-3"></i> Gemar Kode<br />
+              </b>
 
-        <p class="copyright text-muted mt-3 col-lg-11 mx-auto">Kabupaten Pemalang <i class="fa fa-heart text-danger"></i> Indonesia</p>
+              <p className="copyright text-muted mt-3 col-lg-11 mx-auto">Kabupaten Pemalang <i className="fa fa-heart text-danger"></i> Indonesia</p>
 
 
-      </div>
-    </div>
-    </div>
-    </footer>
-        )
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
   }
 }
 
